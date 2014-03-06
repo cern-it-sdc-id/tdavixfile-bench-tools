51f3f0f814fd61706894fc2867d65e632a588803
Adrien Devresse <adevress@cern.ch>
2014-03-06 16:28:38 +0100

    - update  build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-g++ -Wall -Wshadow -std=gnu++11 main.cpp -o test `root-config --libs --cflags` -lTreePlayer -g
+g++ -Wall -Wshadow -std=c++0x main.cpp -o test `root-config --libs --cflags` -lTreePlayer -g
 
-g++ -Wall -Wshadow -std=gnu++11 inflateTree_main.cpp -o testInflate `root-config --libs --cflags` -lTreePlayer -g
+g++ -Wall -Wshadow -std=c++0x  inflateTree_main.cpp -o testInflate `root-config --libs --cflags` -lTreePlayer -g
 
 
-g++ -Wall -Wshadow -std=gnu++11 h2fastnew_main.cpp -o testEvent `root-config --libs --cflags` -lTreePlayer -g 
+g++ -Wall -Wshadow -std=c++0x  h2fastnew_main.cpp -o testEvent `root-config --libs --cflags` -lTreePlayer -g 
 
